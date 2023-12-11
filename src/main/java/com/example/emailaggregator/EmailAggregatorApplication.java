936bdacee338725effb1956d2c6b306a5399a44b
lyuben1337 <lyuben1337@gmail.com>
2023-12-11 08:55:51 +0100

    initial commit

---
+++ src/main/java/com/example/emailaggregator/EmailAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emailaggregator/EmailAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
